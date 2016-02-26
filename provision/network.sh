ed64c17731fe5a6f32540df51ce2180386e01dc1
Michael Schaar <kraeml@sesamestreet.franken.de>
2016-02-26 14:37:25 +0100

    provisions-Shell ohne sudo ausführen

--- provision/network.sh
+++ provision/network.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Netzwerk für Vagrant abschalten"
-sudo /bin/sed -i 's/^auto eth0/\# auto eth0/g' /etc/network/interfaces
+/bin/sed -i 's/^auto eth0/\# auto eth0/g' /etc/network/interfaces
