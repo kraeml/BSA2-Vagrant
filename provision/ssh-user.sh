4d30ddad95c5466e5efd2b1c251650ea2e366dc6
Michael Schaar <kraeml@sesamestreet.franken.de>
2016-03-05 14:56:35 +0100

    SSH für alle Maschinen und DHCP Fehlerbereinigung

--- provision/ssh-user.sh
+++ provision/ssh-user.sh
@@ -1,2 +1,4 @@
 echo "SSH Zugang über privates Netzwerk einrichten"
 cat /vagrant/ssh-config/id_rsa_insecure_rdf.pub >> /home/vagrant/.ssh/authorized_keys
+cp /vagrant/ssh-config/id_rsa_insecure_rdf* /vagrant/ssh-config/config /home/vagrant/.ssh/
+chown -R vagrant /home/vagrant/.ssh
