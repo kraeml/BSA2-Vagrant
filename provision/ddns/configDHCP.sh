5f27858b7cec0daaafc87be1c684255ad7e21359
Michael Schaar <kraeml@sesamestreet.franken.de>
2016-03-05 15:25:31 +0100

    DHCP mit MAC-PC1 angepasst

--- provision/ddns/configDHCP.sh
+++ provision/ddns/configDHCP.sh
@@ -12,7 +12,7 @@ declare -A CONFIG_VARS=( \
   ["NETMASK"]="255.255.255.0" \
   ["RANGE"]="192.168.33.10\ 192.168.33.20" \
   ["_PC1"]="pluto" \
-  ["PC1_HW_ETH"]="00:c0:ee:51:39:9f" \
+  ["PC1_HW_ETH"]="08:00:27:2C:C2:31" \
   ["PC1_ADDR"]="192.168.33.254" \
   ["PC1_DEFAULT_LEASTIME"]="86400000" \
 )
